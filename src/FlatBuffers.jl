db43842bffba65e0e16bac62c7a955adeeba3344
Simeon Schaub <simeondavidschaub99@gmail.com>
2021-10-22 22:58:37 -0400

    fix version bounds for `ismutabletype` (#60)

--- src/FlatBuffers.jl
+++ src/FlatBuffers.jl
@@ -31,7 +31,7 @@ Int8, Int16, Int32, Int64,
 UInt8, UInt16, UInt32, UInt64,
 Float32, Float64}
 
-if VERSION >= v"1.7.0-beta"
+if VERSION >= v"1.7.0-DEV.204"
 	isstruct(T) = isconcretetype(T) && !ismutabletype(T)
 else
 	isstruct(T) = isconcretetype(T) && !T.mutable
