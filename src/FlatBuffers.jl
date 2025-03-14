943f1cca8fbb08e0887784da03e2b17cae959e3f
Jameson Nash <vtjnash@gmail.com>
2025-03-14 15:45:15 -0400

    Update FlatBuffers.jl

--- src/FlatBuffers.jl
+++ src/FlatBuffers.jl
@@ -313,8 +313,9 @@ function FlatBuffers.read(t::Table{T1}, ::Type{T}=T1) where {T1, T}
             push!(args, nullable ? nothing : default(T, TT, T.name.names[i]))
         else
             if isunionvector
-                newr = getvalue(eval(t), eval(o), eval(R))
-                n = length(eval(R)).types
+                R = eval(R)
+                newr = getvalue(t, o, R)
+                n = length(R.types)
                 push!(args, [getfieldvalue(newr, j) for j = 1:n]) 
             else
                 push!(args, getvalue(t, o, R))
